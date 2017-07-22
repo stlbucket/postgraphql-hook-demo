d3b8693e7813e07c140f635a13a98eb7d38c3510
stlbucket <stlbucket@gmail.com>
2017-07-22 10:14:10 -0500

    config change

--- example_config.js
+++ example_config.js
@@ -1,6 +1,6 @@
 process.env.DB_CONNECTION_STRING = '<YOUR DB CONNECTION>'
+process.env.MAILGUN_API_KEY = '<YOUR API KEY>'
+process.env.MAILGUN_DOMAIN = '<YOUR DOMAIN>'
 process.env.PG_DEFAULT_ROLE = 'hook_anonymous'
 process.env.GRAPHIQL = true
 process.env.POSTGRAPHQL_SCHEMA = 'hook'
-process.env.MAILGUN_API_KEY = '<YOUR API KEY>'
-process.env.MAILGUN_DOMAIN = '<YOUR DOMAIN>'
